0b70f61023fc234c5252729c2286d34db209935e
Georg Walther <georgwalther@gmail.com>
2013-01-26 22:06:53 +0000

    final commit before changing frozensets to tuples - necessary because fragments are unique up to and including number of occurrences of each node

--- benchmark.py
+++ benchmark.py
@@ -20,6 +20,17 @@ your_fs = None
 
 b_fragments = None
 y_fragments = None
+b_critical = None
+y_critical = None
+b_critical_extras = None
+y_critical_extras = None
+b_sc = None
+y_sc = None
+b_sg = None
+y_sg = None
+b_ks = None
+y_ks = None
+b_dup = None
 
 def data_summary(data, data_filename):
     print '----------------------'
@@ -28,12 +39,57 @@ def data_summary(data, data_filename):
     print 'number of critical fragments:',str(len([s[ks_i] for s in data if s[ks_i]<0]))
     print '----------------------'
 
+def duplicate_fragments(b_data, b_filename, y_data, y_filename):
+    global b_fragments
+    global y_fragments
+    global b_dup
+
+    b_duplicates = False
+    y_duplicates = False
+
+    if len(b_fragments) < len(b_data):
+        print str(len(b_data)),'fragments in data set',b_filename,'-',str(len(b_fragments)),'unique fragments found'
+        b_duplicates = True
+    elif len(b_fragments) > len(b_data):
+        raise
+    
+    if len(y_fragments) < len(y_data):
+        print str(len(y_data)),'fragments in data set',y_filename,'-',str(len(y_fragments)),'unique fragments found'
+        y_duplicates = True
+    elif len(y_fragments) > len(y_data):
+        raise
+
+    if b_duplicates:
+        b_dup = {(frozenset(s[frag_i][0]), frozenset(s[frag_i][1])): [] for s in b_data}
+        for s in b_data:
+            b_dup[(frozenset(s[frag_i][0]), frozenset(s[frag_i][1]))].append(s)
+    
+    
 def data_compare(b_data, b_filename, y_data, y_filename):
     global b_fragments
     global y_fragments
+    global b_critical
+    global y_critical
+    global b_critical_extras
+    global y_critical_extras
+    global b_sc
+    global y_sc
+    global b_sg
+    global y_sg
+    global b_ks
+    global y_ks
 
     b_fragments = frozenset([(frozenset(s[frag_i][0]), frozenset(s[frag_i][1])) for s in b_data])
     y_fragments = frozenset([(frozenset(s[frag_i][0]), frozenset(s[frag_i][1])) for s in y_data])
+
+    b_sc = {(frozenset(s[frag_i][0]), frozenset(s[frag_i][1])): s[sc_i] for s in b_data}
+    y_sc = {(frozenset(s[frag_i][0]), frozenset(s[frag_i][1])): s[sc_i] for s in y_data}
+
+    b_sg = {(frozenset(s[frag_i][0]), frozenset(s[frag_i][1])): frozenset([frozenset([path for path in sg]) for sg in s[sg_i]]) for s in b_data}
+    y_sg = {(frozenset(s[frag_i][0]), frozenset(s[frag_i][1])): frozenset([frozenset([path for path in sg]) for sg in s[sg_i]]) for s in y_data}
+
+    b_ks = {(frozenset(s[frag_i][0]), frozenset(s[frag_i][1])): s[ks_i] for s in b_data}
+    y_ks = {(frozenset(s[frag_i][0]), frozenset(s[frag_i][1])): s[ks_i] for s in y_data}
 
     print 'set of fragments in',b_filename,'no. of fragments:',len(b_fragments)
     print 'set of fragments in',y_filename,'no. of fragments:',len(y_fragments)
@@ -42,6 +98,16 @@ def data_compare(b_data, b_filename, y_data, y_filename):
     print 'fragments in',b_filename,'but not in',y_filename,':',len(b_fragments.difference(y_fragments))
     print 'fragments in',y_filename,'but not in',b_filename,':',len(y_fragments.difference(b_fragments))
 
+    b_critical = frozenset([((frozenset(s[frag_i][0]), frozenset(s[frag_i][1])), frozenset([frozenset([path for path in sg]) for sg in s[sg_i]]), s[ks_i]) for s in b_data if s[ks_i]<0.0])
+
+    y_critical = frozenset([((frozenset(s[frag_i][0]), frozenset(s[frag_i][1])), frozenset([frozenset([path for path in sg]) for sg in s[sg_i]]), s[ks_i]) for s in y_data if s[ks_i]<0.0])
+
+    y_critical_extras = list(y_critical.difference(b_critical))
+    b_critical_extras = list(b_critical.difference(y_critical))
+
+    for yf in y_critical_extras:
+        print 'sg in b:',len(b_sg[yf[0]]),', sg in y:',len(y_sg[yf[0]])
+    
 def main():
     # declare global variables
     global benchmark_file_name
