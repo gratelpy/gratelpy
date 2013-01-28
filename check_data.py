2e74d01d34ccc5b8a4deae6db25630de844152d8
Georg Walther <georgwalther@gmail.com>
2013-01-28 16:31:16 +0000

    implemented check of K_S

--- check_data.py
+++ check_data.py
@@ -8,10 +8,13 @@ import sys
 from collections import Counter
 import errno
 from graph import get_path_graph, get_valid_path_graph_cycles
-from subgraphs import get_subgraph_motifs
+from subgraphs import get_subgraph_motifs, score_subgraph
+from fragments import score_fragment
 
 import networkx as nx
 import itertools as it
+
+import math
 
 # data format
 frag_i = 0
@@ -136,3 +139,59 @@ for frag in critical_fragments_unique:
             print str(subgraphs_tested),'of',str(no_subgraphs),'tested -',100*float(subgraphs_tested)/float(no_subgraphs),'%'
 
 print str(subgraphs_tested),'subgraphs tested and',str(subgraphs_with_all_paths_in_cycles),'of these have all paths in cycles'
+
+# check that fragment score K_S is correct for every fragment
+# we read out the K_S from data, generate K_S anew using the current library method, and use our new method using sg_motifs
+fragments_checked = 0
+k_s_ok = 0
+for frag in critical_fragments_unique:
+#frag = critical_fragments_unique[0]
+    fragments_checked += 1
+    sc = subgraph_components[frag]
+    sg_motifs = get_subgraph_motifs(subgraph_components[frag])
+    k_s_frag_this_method = 0
+    k_s_frag_library_method = score_fragment(subgraphs[frag], sc, frag)
+    k_s_frag_library_method_redone = 0
+
+    for sg in subgraphs[frag]:
+        k_g = float(1)
+        t_g = len(sg_motifs[frozenset(sg)]['cycles'])
+        k_g = k_g * math.pow(-1, t_g)
+        for edge in sg_motifs[frozenset(sg)]['edges']:
+            # NOTE: assuming that all stoichiometric coefficients == 1
+            k_g = k_g * 1
+        for cycle in sg_motifs[frozenset(sg)]['cycles']:
+            for path in cycle:
+                if path in sc[path[0]]['n_paths'] and path not in sc[path[0]]['p_paths']:
+                    # NOTE: assuming that all stoichiometric coefficients == 1
+                    k_g = k_g * (-1)
+                elif path not in sc[path[0]]['n_paths'] and path in sc[path[0]]['p_paths']:
+                    # NOTE: assuming that all stoichiometric coefficients == 1
+                    k_g = k_g * 1
+                else:
+                    raise
+
+        k_g_library_method = score_subgraph([tuple(sg), sc])
+        if k_g != k_g_library_method:
+            print 'k_g this method =',str(k_g)
+            print 'k_g library method =',str(k_g_library_method)
+            print 'subgraph =',str(sg)
+            raise
+
+        k_s_frag_this_method += k_g
+        k_s_frag_library_method_redone += k_g_library_method
+
+    different_K_S = [k_s_frag_this_method, k_s_frag_library_method[-1], k_s_frag_library_method_redone, ks[frag]]
+    if not all(ks_1 == ks_2 for ks_1, ks_2 in zip(different_K_S, different_K_S)):
+        print 'fragment',str(frag) 
+        print 'K_S this method =',str(k_s_frag_this_method)
+        print 'K_S library method =',str(k_s_frag_library_method[-1])
+        print 'K_S library method redone =',str(k_s_frag_library_method_redone)
+        print 'K_S in saved data =',str(ks[frag])
+    else:
+        k_s_ok += 1
+
+    if fragments_checked % 10 == 0:
+        print str(fragments_checked),'of',str(len(critical_fragments_unique)),'fragments checked -',str(100*float(fragments_checked)/float(len(critical_fragments_unique))),'%'
+
+print str(fragments_checked),'fragments checked of total',str(len(critical_fragments_unique)),'fragments. k_s scores ok:',str(k_s_ok)
