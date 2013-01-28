5d2f588fab912e30d353d49611fbf864fb81b81e
Georg Walther <georgwalther@gmail.com>
2013-01-28 09:54:37 +0000

    finished implementing test of whether every path in every subgraph is within a cycle

--- check_data.py
+++ check_data.py
@@ -95,23 +95,44 @@ for f in critical_fragments_unique:
             raise
 
 # check that every path in every subgraph is in a cycle
-frag = critical_fragments_unique[0]
-path_graph = get_path_graph(subgraph_components[frag])
-cycles = get_valid_path_graph_cycles(path_graph)
-sc = subgraph_components[frag]
+subgraphs_tested = 0
+subgraphs_with_all_paths_in_cycles = 0
+no_subgraphs = sum([1 for frag in critical_fragments_unique for sg in subgraphs[frag]])
+print 'checking if all paths in every subgraph are witihin a cycle ...'
+for frag in critical_fragments_unique:
+#frag = critical_fragments_unique[0]
+    path_graph = get_path_graph(subgraph_components[frag])
+    cycles = get_valid_path_graph_cycles(path_graph)
+    sc = subgraph_components[frag]
+    
+    # create cycle graph
+    cycles_in_path_graph_1 = get_valid_path_graph_cycles(path_graph)
+    cycles_in_path_graph_2 = None
+    cycle_graph = nx.Graph()
+    cycles_in_path_graph = get_valid_path_graph_cycles(path_graph)
+    for cycle in cycles_in_path_graph:
+        cycle_graph.add_node(tuple(cycle))
+    for cycle_1 in cycles_in_path_graph_1:
+        cycles_in_path_graph_2 = get_valid_path_graph_cycles(path_graph)
+        for cycle_2 in cycles_in_path_graph_2:
+            if cycle_1 is not cycle_2 and any(path_in_cycle_1[0] in [path_in_cycle_2[0] for path_in_cycle_2 in cycle_2] for path_in_cycle_1 in cycle_1):
+                cycle_graph.add_edge(tuple(cycle_1),tuple(cycle_2)) # nodes must be immutable objects, hence create tuple from list
+    cycle_graph = nx.complement(cycle_graph)
 
-# create cycle graph
-cycles_in_path_graph_1 = get_valid_path_graph_cycles(path_graph)
-cycles_in_path_graph_2 = None
-cycle_graph = nx.Graph()
-cycles_in_path_graph = get_valid_path_graph_cycles(path_graph)
-for cycle in cycles_in_path_graph:
-    cycle_graph.add_node(tuple(cycle))
-for cycle_1 in cycles_in_path_graph_1:
-    cycles_in_path_graph_2 = get_valid_path_graph_cycles(path_graph)
-    for cycle_2 in cycles_in_path_graph_2:
-        if cycle_1 is not cycle_2 and any(path_in_cycle_1[0] in [path_in_cycle_2[0] for path_in_cycle_2 in cycle_2] for path_in_cycle_1 in cycle_1):
-            cycle_graph.add_edge(tuple(cycle_1),tuple(cycle_2)) # nodes must be immutable objects, hence create tuple from list
-cycle_graph = nx.complement(cycle_graph)
+    sg_motifs = get_subgraph_motifs(subgraph_components[frag])
+    for s in subgraphs[frag]:
+        subgraphs_tested += 1
+        s_sg_motifs = sg_motifs[frozenset(s)]
+        all_paths_in_cycles = all([s_el in [path for cycle in s_sg_motifs['cycles'] for path in cycle] for s_el in s if len(s_el)==3])
+        if all_paths_in_cycles:
+            subgraphs_with_all_paths_in_cycles += 1
+        else:
+            print 'fragment',frag
+            print 'subgraph',s
+            print 'subgraph motifs',s_sg_motifs
+            raise Exception('not all paths in cycles')
 
-sg_motifs = get_subgraph_motifs(subgraph_components[frag])
+        if subgraphs_tested%10==0:
+            print str(subgraphs_tested),'of',str(no_subgraphs),'tested -',float(subgraphs_tested)/float(no_subgraphs),'%'
+
+print str(subgraphs_tested),'subgraphs tested and',str(subgraphs_with_all_paths_in_cycles),'of these have all paths in cycles'
