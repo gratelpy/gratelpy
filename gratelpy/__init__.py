661a9a909d2edb9e47867c2d9967bc61834d887e
Georg Walther <georgwalther@gmail.com>
2013-09-27 16:20:48 +0100

    refactor code to meet PEP8

--- gratelpy/__init__.py
+++ gratelpy/__init__.py
@@ -1,4 +1,8 @@
-VERSION = (0,1,1)
+"""GraTeLPy: Graph-Theoretic Linear Stability Analysis"""
 
-def get_version(*args, **kwargs):
-	return str(VERSION[0])+'.'+str(VERSION[1])+'.'+str(VERSION[2])
+VERSION = (0, 1, 1)
+
+def get_version():
+    """Returns the current version of GraTeLPy."""
+
+    return str(VERSION[0])+'.'+str(VERSION[1])+'.'+str(VERSION[2])
