e8f7a541efbedc43bb889bcb3347597db7480575
Georg Walther <georgwalther@gmail.com>
2013-11-09 15:10:52 +0000

    include __init__ of tests submodule

---
+++ gratelpy/tests/__init__.py
@@ -0,0 +1,18 @@
+import unittest
+import sys
+
+from test_reversible_substrate_inhibition import TestReversibleSubstrateInhibition
+
+def runtests():
+    suite = unittest.TestSuite()
+    
+    # add TestCase objects
+    suite.addTest(TestReversibleSubstrateInhibition('test_alpha'))
+    suite.addTest(TestReversibleSubstrateInhibition('test_beta'))
+    suite.addTest(TestReversibleSubstrateInhibition('test_species'))
+    suite.addTest(TestReversibleSubstrateInhibition('test_constants'))
+    suite.addTest(TestReversibleSubstrateInhibition('test_fragments'))
+    suite.addTest(TestReversibleSubstrateInhibition('test_subgraphs'))
+
+    failures = unittest.TextTestRunner().run(suite)
+    sys.exit(failures)
