28f20f914a5b6d78b231ddd0f09faf4b61372203
Georg Walther <georgwalther@gmail.com>
2013-11-18 12:20:18 +0000

    emulate server / client interaction

--- gratelpy/tests/test_fragment_server.py
+++ gratelpy/tests/test_fragment_server.py
@@ -4,7 +4,9 @@ import os
 path = os.path.split(os.path.realpath(__file__))[0]
 
 from multiprocessing import freeze_support
-from gratelpy.gensg import gen_valid_subgraphs_mps
+from multiprocessing.managers import SyncManager
+import Queue
+from gratelpy.gensg import make_server_manager
 from gratelpy.parse_mechanism import get_network_from_mechanism
 from gratelpy.stoich import get_graph_stoich
 from gratelpy.fragments import get_sensible_fragments
@@ -20,7 +22,51 @@ def main():
     G, stoich, stoich_rank = get_graph_stoich(alpha, beta)
     valid_fragments = get_sensible_fragments(G, stoich_rank)
 
-    gen_valid_subgraphs_mps(G, valid_fragments, 4)
+    # we will emulate the behavior of both gratelpy_fragment_server
+    # and gratelpy_subclient here
+
+    # server sets up fragment queue
+    server_manager = make_server_manager(50000, 'smallg')
+    server_fragment_q = server_manager.get_fragment_q()
+    server_valid_frag_q = server_manager.get_valid_frag_q()
+    for f in valid_fragments:
+        print('Putting fragment %s in queue' % str(f))
+        server_fragment_q.put(f)
+    print('Fragment queue filled.')
+
+    # client fetches all fragments one-by-one and processes them
+    class QueueManager(SyncManager): pass    
+    QueueManager.register('get_fragment_q')
+    QueueManager.register('get_valid_frag_q')
+    
+    client_manager = QueueManager(address=('localhost', 50000), 
+                                  authkey='smallg')
+    print('Client connecting ...')
+    client_manager.connect()
+    print('Client connected.')
+
+    client_fragment_q = client_manager.get_fragment_q()
+    client_valid_frag_q = client_manager.get_valid_frag_q()
+
+    print('Client starts fetching fragments off the queue ...')
+    while True:
+        try:
+            f = client_fragment_q.get_nowait()
+            print('Client fetched fragment %s.' % str(f))
+            client_valid_subgraphs = get_all_valid_subgraphs(G, 
+                                                             stoich_rank, 
+                                                             f)
+            client_valid_frag_q.put(client_valid_subgraphs)
+        except Queue.Empty:
+            break
+
+    # now let the server go through the list of data deposited by the client
+    while True:
+        try:
+            server_valid_subgraphs = server_valid_frag_q.get()
+            print server_valid_subgraphs
+        except:
+            break
 
 if __name__ == '__main__':
     # Required on Windows 
