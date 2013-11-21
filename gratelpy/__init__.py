aef9b43895938abd4fbfcff90b03c34860a260cf
Georg Walther <georgwalther@gmail.com>
2013-11-21 20:33:35 +0000

    implement method that analyzes an entire mechanism on a single host with variable number of 'clients'

--- gratelpy/__init__.py
+++ gratelpy/__init__.py
@@ -1,4 +1,31 @@
 VERSION = (0,1,1)
 
+from multiprocessing import Pool
+from gratelpy.parse_mechanism import get_network_from_mechanism
+from gratelpy.stoich import get_graph_stoich
+from gratelpy.fragments import get_sensible_fragments
+from gratelpy.subgraphs import get_all_valid_subgraphs
+from functools import partial
+import os
+
 def get_version(*args, **kwargs):
 	return str(VERSION[0])+'.'+str(VERSION[1])+'.'+str(VERSION[2])
+
+def analyze(name, no_species, no_proc=1):
+
+    pool = Pool(no_proc)
+
+    alpha, beta, dict_complexes, dict_constants, dict_complexes_reverse, \
+        dict_constants_reverse = get_network_from_mechanism(name, no_species)
+			
+    G, stoich, stoich_rank = get_graph_stoich(alpha, beta)
+    valid_fragments = get_sensible_fragments(G, stoich_rank)
+
+    get_subgraphs = partial(get_all_valid_subgraphs, G, stoich_rank)
+
+    results = []
+    r = pool.map_async(get_subgraphs, valid_fragments, callback=results.append)
+    r.wait()
+
+    return results[0]
+
