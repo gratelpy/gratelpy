b0c4c11dd70db74225c82715c2dfcf35458c260e
Georg Walther <georgwalther@gmail.com>
2013-12-11 14:22:46 +0000

    pep8 cleanup

--- gratelpy/utils.py
+++ gratelpy/utils.py
@@ -16,17 +16,22 @@ rxn_i = 1
 def result_get_fragment(result):
     return result[0]
 
+
 def result_get_ks(result):
     return result[-1]
+
 
 def result_get_sc(result):
     return result[1]
 
+
 def result_get_sg(result):
     return result[2]
 
+
 def fragment_get_species(fragment):
     return fragment[0]
+
 
 def fragment_get_reactions(fragment):
     return fragment[1]
@@ -35,8 +40,10 @@ def fragment_get_reactions(fragment):
 def species_get_index(species):
     return int(species[1:])-1
 
+
 def reaction_get_index(reaction):
     return int(reaction[1:])-1
+
 
 def edge_get_species(edge):
     return edge[0]
