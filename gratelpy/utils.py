183e6690a3d3d06074f12163dca177e3297174fc
Georg Walther <georgwalther@gmail.com>
2013-12-11 10:15:28 +0000

    Add helper functions to reduce reliance on format choice Early on in development I decided to call species and reactions in certain ways, choose certain data types and formats for fragments, edges, etc. These helper functions are intended as a first step towards making the code less reliant on these early decisions.

--- gratelpy/utils.py
+++ gratelpy/utils.py
@@ -3,3 +3,31 @@ def invert_dict(a_dict):
     for key in a_dict.keys():
         rev_dict[a_dict[key]] = key
     return rev_dict
+
+def result_get_fragment(result):
+    return result[0]
+
+def result_get_ks(result):
+    return result[-1]
+
+def result_get_sc(result):
+    return result[1]
+
+def result_get_sg(result):
+    return result[2]
+
+def fragment_get_species(fragment):
+    return fragment[0]
+
+def fragment_get_reactions(fragment):
+    return fragment[1]
+
+# index in alpha, beta, and stoichiometry matrices
+def species_get_index(species):
+    return int(species[1:])-1
+
+def reaction_get_index(reaction):
+    return int(reaction[1:])-1
+
+def edge_get_species(edge):
+    return edge[0]
